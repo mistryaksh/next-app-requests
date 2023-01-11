2b814fe22a0a7f7365b2d81b5175a4c650f45d95
mistryaksh <mistryaksh1998@gmail.com>
2023-01-11 14:47:37 +0530

    update api file

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string
-}
+const BackendUrl = `https://swayamhealth.info/api`;
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const response = await fetch(
+    `${BackendUrl}/category/gettestfromkiosk/MUMSAN2`,
+    {
+      method: "GET",
+    }
+  );
+  const data = await response.json();
+  console.log("data", data);
+  res.status(200).json(data);
 }
