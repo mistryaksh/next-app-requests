84d78dbfcc3950a08c1620b1386460054234450c
mistryaksh <mistryaksh1998@gmail.com>
2023-01-11 14:40:33 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'ORIGIN',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'PUT, GET, HEAD, POST, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'include',
+          },
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+          {
+            key: 'origin',
+            value: 'https://react-test-app-1.netlify.app/',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
