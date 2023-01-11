a35ac3223585ba156a8689a3326933f1a9849eaa
mistryaksh <mistryaksh1998@gmail.com>
2023-01-11 14:49:14 +0530

    update something

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
 const BackendUrl = `https://swayamhealth.info/api`;
 
-export default function App(props: any) {
-  console.log(props.data.data.result);
-  return <div>Request type - {props.data.type}</div>;
+export default function App() {
+  return <div>Request type - </div>;
 }
 
-export async function getServerSideProps() {
-  const res = await fetch(`${BackendUrl}/category/gettestfromkiosk/MUMSAN2`, {
-    method: "GET",
-  });
-  const data = await res.json();
-  return { props: { data } };
-}
+// export async function getServerSideProps() {
+//   const res = await fetch(`${BackendUrl}/category/gettestfromkiosk/MUMSAN2`, {
+//     method: "GET",
+//   });
+//   const data = await res.json();
+//   return { props: { data } };
+// }
